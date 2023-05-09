1c9dc73c735c8b4d7d6b4f5f4d2f17df5547f1e5
Taleem <taleemmankuer@gmail.com>
2023-05-09 05:19:08 +0300

    updated navbar

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("something");
+const menu = document.querySelector(".menu");
+const list = document.querySelector("ul");
+const nav = document.querySelector("nav");
+
+list.style.top = "-280px";
+
+menu.addEventListener("click", () => {
+  if (list.style.top == "-250px") {
+    list.style.top = "80px";
+  } else {
+    list.style.top = "-250px";
+  }
+});
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 121) {
+    nav.classList.add("navbar");
+  } else {
+    nav.classList.remove("navbar");
+  }
+});
